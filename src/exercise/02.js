d7bbf218c4e97025941c73e6f09e72d030954cd0
Julaxe <julaxe8@gmail.com>
2021-06-26 15:43:53 -0400

    2.3 custom hooks done

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,27 +3,42 @@
 
 import * as React from 'react'
 
+function useLocalStorageState(key, defaultValue = '') {
+  const [state, setState] = React.useState(
+    () => window.localStorage.getItem(key) || defaultValue
+  )
+
+  React.useEffect(() => {
+    window.localStorage.setItem(key, state)
+  }, [state, key])
+
+  return [state, setState]
+
+}
+
 function Greeting({initialName = ''}) {
  
-  const [name, setName] = React.useState(
-    () => window.localStorage.getItem('name') || initialName
-    )
-  // we declare an array as a second argument so we tell to useEffect when to update, in this case
-  // we are telling useEffect that only updates when the variable 'name' is changed.
-  React.useEffect(() => {
-    window.localStorage.setItem('name', name)
-  }, [name])
+  const [name, setName] = useLocalStorageState('name', initialName)
+  const [middleName, setMiddleName] = useLocalStorageState('middleName')
 
-  function handleChange(event) {
+  function handleChange1(event) {
     setName(event.target.value)
+  }
+  function handleChange2(event) {
+    setMiddleName(event.target.value)
   }
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input value={name} onChange={handleChange} id="name" />
+        <input value={name} onChange={handleChange1} id="name" />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+      <form>
+        <label htmlFor="middleName">Middle Name: </label>
+        <input value={middleName} onChange={handleChange2} id="middleName" />
+      </form>
+      {middleName ? <strong>Hello {middleName}</strong> : 'Please type your middleName'}
     </div>
   )
 }
